e82712d31e6511a9528241b7972c167bb87fe954
socratic617 <erik.solis02135@gmail.com>
2023-12-20 05:05:14 -0500

    fe-erik created about me for landing pg

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,10 @@
 export default function About() {
     return (
         <div>
-            <p>About here</p>
+            <h2>About Us</h2>
+            <img src="#"/>
+            <p>We are committed to cultivating a resilient and interconnected community through grassroots efforts to address food insecurity. At the heart of our initiative are community fridges—accessible, community-driven spaces stocked with fresh, free food for those in need.</p>
+            <img src="#"/>
         </div>
     )
 }
