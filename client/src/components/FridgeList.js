215edfa50e62579317f59b77f37fb33f4f524b76
socratic617 <erik.solis02135@gmail.com>
2023-12-21 00:31:46 -0500

    fe-Erik imported FridgeCard component and mapped through card component for each fridge to display w/ test data for fridge list

--- client/src/components/FridgeList.js
+++ client/src/components/FridgeList.js
@@ -1,13 +1,15 @@
 import FridgeCard from './FridgeCard.js'
+import React from "react"
 
 export default function FridgeList({data}) {
     console.log("FridgeList data :")
     console.log(data)
     return (
         <div className='card-list'>
-        
-
-                
+            {data.map((item, index) => (
+                // item refers to object holding all keys and values 
+                <FridgeCard data={item} />
+            ))} 
         </div>
     )
 }
