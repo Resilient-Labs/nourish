de4bf5e9005e332a80489a9bcee09550b47101ed
socratic617 <erik.solis02135@gmail.com>
2023-12-21 00:34:27 -0500

    fe-erik imported features to get interactive map going, added philly marker on map, the zoom on location for area of fridges, and divs for basic styling containers for fridge list and fridge map

--- client/src/components/FridgeMap.js
+++ client/src/components/FridgeMap.js
@@ -1,8 +1,33 @@
+
+import 'leaflet/dist/leaflet.css'
+import {
+    MapContainer,
+    TileLayer,
+    useMap,
+    Marker,
+    Popup
+} from 'react-leaflet'
+
 export default function FridgeMap() {
-    return (
-        <div>
-            INTERACTIVE MAP PLACEHOLDER
-            <iframe/>
+    const position = [39.933026, -75.174741]
+return (
+    <div className="leaflet-container-outside">
+            <div className="leaflet-container-list">    
+                <p>hello</p>
+            </div>
+            <div className="leaflet-container">
+                <MapContainer center={position} zoom={15} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
         </div>
+    </div>
     )
 }
