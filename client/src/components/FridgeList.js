15775e936997e0973551ac72129436bb364c9ab2
socratic617 <erik.solis02135@gmail.com>
2023-12-21 15:51:53 -0500

    fe-erik added container for fridgelist to be nested in frdige parent container

--- client/src/components/FridgeList.js
+++ client/src/components/FridgeList.js
@@ -1,15 +1,20 @@
 import FridgeCard from './FridgeCard.js'
 import React from "react"
+// import {Button} from 'flowbite-react';
+
+
 
 export default function FridgeList({data}) {
     console.log("FridgeList data :")
     console.log(data)
     return (
-        <div className='card-list'>
-            {data.map((item, index) => (
-                // item refers to object holding all keys and values 
-                <FridgeCard data={item} />
-            ))} 
+        <div className="leaflet-container-list">    
+            <div className='card-list '>
+                {data.map((item, index) => (
+                    // item refers to object holding all keys and values 
+                    <FridgeCard data={item} />
+                ))}
+            </div> 
         </div>
     )
 }
