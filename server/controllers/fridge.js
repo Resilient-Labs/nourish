f95dd3241abbb1c3b586e5ad0228e29d755aef4b
JacinthaDev <jacinthadev8@gmail.com>
2023-12-20 20:45:37 -0500

    add getAllFridges controller and route

--- server/controllers/fridge.js
+++ server/controllers/fridge.js
@@ -2,14 +2,23 @@ const cloudinary = require("../middleware/cloudinary");
 const Fridge = require("../models/FridgeProfile");
 
 module.exports = {
-    getFridgesbyZip: async (req, res) => {
+    getAllFridges: async (req, res) => {
         try {
-            //=================Do we want to do req.params.zipcode or reqbodyzipcode?
-            const fridges = await Fridge.find({ user: req.body.zipcode });
-            res.json({ fridges: fridges });
+            const fridges = await Fridge.find().lean();
+            res.json({ FRIDGES: fridges });
         } catch (err) {
             console.log(err);
             res.status(500).json({ message: "Internal Server Error" });
+        }
+    },
+    getFridgesbyZip: async (req, res) => {
+        try {
+            //=================Do we want to do req.params.zipcode or reqbodyzipcode?
+            const fridges = await Fridge.find({ zipCode: req.body.ZIPCODE });
+            res.json({ FRIDGES: fridges });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: "No fridges found in that zipcode" });
         }
     },
 };
