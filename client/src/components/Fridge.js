4244f71cb00041c058b446fa287e6fa5b15ecfb0
socratic617 <erik.solis02135@gmail.com>
2023-12-22 04:36:20 -0500

    fe-erik Fridge.js : added styling to div container and added padding

--- client/src/components/Fridge.js
+++ client/src/components/Fridge.js
@@ -4,13 +4,11 @@ import FridgeMap from './FridgeMap.js';
 
 export default function Fridge({ data }) {
     return (
-        <div style={{ display: 'flex' }}>
+        <div className="flex p-12">
             <div style={{ flex: 1, marginRight: '20px' }}>
-                <p>Fridge list here</p>
                 <FridgeList data={data} />
             </div>
             <div style={{ flex: 1 }}>
-                <p>Fridge map here</p>
                 <FridgeMap data={data} />
             </div>
         </div>
