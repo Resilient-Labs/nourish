27c462bd143c16ec266c304b835e30be6c85b4b2
brianna willis <brianna.willis195@gmail.com>
2023-12-22 15:02:41 -0500

    adding prettier for formatting

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,90 +1,80 @@
-const passport = require("passport");
-const validator = require("validator");
-const User = require("../models/User");
-const Event = require("../models/Event");
-
-
+const passport = require("passport")
+const validator = require("validator")
+const User = require("../models/User")
+const Event = require("../models/Event")
 
 exports.getLogin = (req, res) => {
   if (req.user) {
-    return res.redirect("/profile");
+    return res.redirect("/profile")
   }
   res.render("login", {
-    title: "Login", pageName: 'login'
-  });
-};
-
-
-
+    title: "Login",
+    pageName: "login"
+  })
+}
 
 exports.postLogin = (req, res, next) => {
-  const validationErrors = [];
+  const validationErrors = []
   if (!validator.isEmail(req.body.email))
-    validationErrors.push({ msg: "Please enter a valid email address." });
+    validationErrors.push({ msg: "Please enter a valid email address." })
   if (validator.isEmpty(req.body.password))
-    validationErrors.push({ msg: "Password cannot be blank." });
+    validationErrors.push({ msg: "Password cannot be blank." })
 
   if (validationErrors.length) {
-    req.flash("errors", validationErrors);
-    return res.redirect("/login");
+    req.flash("errors", validationErrors)
+    return res.redirect("/login")
   }
   req.body.email = validator.normalizeEmail(req.body.email, {
-    gmail_remove_dots: false,
-  });
+    gmail_remove_dots: false
+  })
 
   passport.authenticate("local", (err, user, info) => {
     if (err) {
-      return next(err);
+      return next(err)
     }
     if (!user) {
-      req.flash("errors", info);
-      return res.redirect("/login");
+      req.flash("errors", info)
+      return res.redirect("/login")
     }
     req.logIn(user, (err) => {
       if (err) {
-        return next(err);
+        return next(err)
       }
-      console.log('logging in')
-      req.flash("success", { msg: "Success! You are logged in." });
-      res.redirect(req.session.returnTo || "/profile");
-    });
-  })(req, res, next);
-};
-
-
+      console.log("logging in")
+      req.flash("success", { msg: "Success! You are logged in." })
+      res.redirect(req.session.returnTo || "/profile")
+    })
+  })(req, res, next)
+}
 
 exports.logout = (req, res) => {
   req.logout(() => {
-    console.log('User has logged out.')
+    console.log("User has logged out.")
   })
   req.session.destroy((err) => {
     if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
-  });
-};
-
+      console.log("Error : Failed to destroy the session during logout.", err)
+    req.user = null
+    res.redirect("/")
+  })
+}
 
 exports.getSignup = (req, res) => {
   if (req.user) {
-    return res.redirect("/profile");
+    return res.redirect("/profile")
   }
   res.render("signup", {
-    title: "Create Account", pageName: 'signup'
-  });
-};
-
-
+    title: "Create Account",
+    pageName: "signup"
+  })
+}
 
 exports.deleteAccount = async (req, res) => {
   try {
-
-
     //if user is a nursing home account delete all events created by them
 
     if (req.user.role.isNursingHome === true) {
-      await Event.deleteMany({ user: req.user.id });
+      await Event.deleteMany({ user: req.user.id })
     }
 
     //if user is a volunteers, delete them from all events they signed up for an update the events numsNeeded
@@ -92,70 +82,69 @@ exports.deleteAccount = async (req, res) => {
     if (req.user.role.isVolunteer === true) {
       let events = []
       // Fetch events the user has signed up for
-      events = await Event.find({ volunteers: req.user.id });
+      events = await Event.find({ volunteers: req.user.id })
 
       // Update numNeeded for each event
       for (const event of events) {
         await Event.findByIdAndUpdate(
           event._id,
-          { $inc: { numNeeded: + 1 }, $pull: { volunteers: req.user._id } },
+          { $inc: { numNeeded: +1 }, $pull: { volunteers: req.user._id } },
           { new: true }
-        );
-        const updatedEvents = await Event.find({ volunteers: req.user.id });
-        console.log('Updated Events after User Deletion', updatedEvents);
+        )
+        const updatedEvents = await Event.find({ volunteers: req.user.id })
+        console.log("Updated Events after User Deletion", updatedEvents)
       }
-      console.log('Events Updated before User Deletion')
+      console.log("Events Updated before User Deletion")
     }
 
     // Delete the user account
-    await User.findByIdAndDelete(req.user._id);
-
+    await User.findByIdAndDelete(req.user._id)
 
     req.logout(() => {
-      console.log("User has logged out.");
-    });
+      console.log("User has logged out.")
+    })
     req.session.destroy((err) => {
       if (err) {
-        console.log("Error: Failed to destroy the session during logout.", err);
+        console.log("Error: Failed to destroy the session during logout.", err)
       }
-      req.user = null;
+      req.user = null
 
-      res.redirect("/");
-    });
+      res.redirect("/")
+    })
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
+    console.error(err)
+    res.status(500).send("Internal Server Error")
   }
-};
+}
 
 exports.postSignup = (req, res, next) => {
-  const validationErrors = [];
+  const validationErrors = []
   if (!validator.isEmail(req.body.email))
-    validationErrors.push({ msg: "Please enter a valid email address." });
+    validationErrors.push({ msg: "Please enter a valid email address." })
   if (!validator.isLength(req.body.password, { min: 8 }))
     validationErrors.push({
-      msg: "Password must be at least 8 characters long",
-    });
+      msg: "Password must be at least 8 characters long"
+    })
   if (req.body.password !== req.body.confirmPassword)
-    validationErrors.push({ msg: "Passwords do not match" });
+    validationErrors.push({ msg: "Passwords do not match" })
 
   if (validationErrors.length) {
-    req.flash("errors", validationErrors);
-    return res.redirect("../signup");
+    req.flash("errors", validationErrors)
+    return res.redirect("../signup")
   }
   req.body.email = validator.normalizeEmail(req.body.email, {
-    gmail_remove_dots: false,
-  });
+    gmail_remove_dots: false
+  })
 
-  if (req.body.userRole === 'volunteer') {
-    console.log('Volunteer')
+  if (req.body.userRole === "volunteer") {
+    console.log("Volunteer")
   } else {
-    console.log('Nursing Home')
+    console.log("Nursing Home")
   }
 
   //check if user signed up as volunteer or nursing home
 
-  let isVolunteer = (req.body.userRole === 'volunteer') ? true : false
+  let isVolunteer = req.body.userRole === "volunteer" ? true : false
 
   const user = new User({
     userName: req.body.userName,
@@ -163,9 +152,9 @@ exports.postSignup = (req, res, next) => {
     password: req.body.password,
     role: {
       isVolunteer: isVolunteer,
-      isNursingHome: !isVolunteer,
+      isNursingHome: !isVolunteer
     }
-  });
+  })
 
   //Check to make sure there are no users with the same email or same username
 
@@ -173,25 +162,25 @@ exports.postSignup = (req, res, next) => {
     { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
     (err, existingUser) => {
       if (err) {
-        return next(err);
+        return next(err)
       }
       if (existingUser) {
         req.flash("errors", {
-          msg: "Account with that email address or username already exists.",
-        });
-        return res.redirect("../signup");
+          msg: "Account with that email address or username already exists."
+        })
+        return res.redirect("../signup")
       }
       user.save((err) => {
         if (err) {
-          return next(err);
+          return next(err)
         }
         req.logIn(user, (err) => {
           if (err) {
-            return next(err);
+            return next(err)
           }
-          res.redirect("/createprofile");
-        });
-      });
+          res.redirect("/createprofile")
+        })
+      })
     }
-  );
-};
+  )
+}
--- server/controllers/fridge.js
+++ server/controllers/fridge.js
@@ -1,40 +1,42 @@
-import { cloudinary } from "../middleware/cloudinary.js";
+import { cloudinary } from "../middleware/cloudinary.js"
 import { CommunityFridge as Fridge } from "../models/CommunityFridge.js"
-import pkg from 'mongodb';
-const { ObjectID } = pkg;
+import pkg from "mongodb"
+const { ObjectID } = pkg
 
-export const getAllFridges =  async (req, res) => {
-    try {
-        const fridges = await Fridge.find().lean();
-        res.json({ fridges: fridges });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+export const getAllFridges = async (req, res) => {
+  try {
+    const fridges = await Fridge.find().lean()
+    res.json({ fridges: fridges })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: "Internal Server Error" })
+  }
 }
 
 export const getFridgesbyZip = async (req, res) => {
-    try {
-        // const newFridge = new Fridge({"name": "Fridge1", "isFridge": true, "isPantry": false})
-        // newFridge.save()
-        // .then(() => console.log("saved"))   
-        // .catch((err) => console.error("Error in getFridgesbyZip:", err))
-        const fridges = await Fridge.find({ 'location.zipCode': req.params.zipcode });
-        console.log(fridges)
-        res.json({ fridges: fridges });
-    } catch (err) {
-        console.error("Error in getFridgesbyZip:", err);
-        res.status(500).json({ message: "No fridges found in that zipcode" });
-    }
+  try {
+    // const newFridge = new Fridge({"name": "Fridge1", "isFridge": true, "isPantry": false})
+    // newFridge.save()
+    // .then(() => console.log("saved"))
+    // .catch((err) => console.error("Error in getFridgesbyZip:", err))
+    const fridges = await Fridge.find({
+      "location.zipCode": req.params.zipcode
+    })
+    console.log(fridges)
+    res.json({ fridges: fridges })
+  } catch (err) {
+    console.error("Error in getFridgesbyZip:", err)
+    res.status(500).json({ message: "No fridges found in that zipcode" })
+  }
 }
 
-export const getFridge =  async (req, res) => {
-    try {
-        const fridge = await Fridge.findById(req.params.id);
-        console.log(fridge)
-        res.json({ fridge: fridge });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "No fridge found" });
-    }
+export const getFridge = async (req, res) => {
+  try {
+    const fridge = await Fridge.findById(req.params.id)
+    console.log(fridge)
+    res.json({ fridge: fridge })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: "No fridge found" })
+  }
 }
