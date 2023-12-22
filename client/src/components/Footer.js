db023cdf76b67931ec4b243422c253b66dfa9120
socratic617 <erik.solis02135@gmail.com>
2023-12-22 01:51:06 -0500

    fe-sarah multple sections landing page: styled and incorperated content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,20 @@
-export default function Footer() {
-    return (
-        <div>
-            <p>Footer goes here</p>
+'use client';
+import { Footer } from 'flowbite-react';
+
+export default function Component() {
+  return (
+    <Footer container className="px-10">
+      <div className="w-full text-center">
+        <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
+          <Footer.Brand href="https://nourish.com" name="Nourish"/>
+          <Footer.LinkGroup>
+            <Footer.Link href="#">About</Footer.Link>
+            <Footer.Link href="#">Contact</Footer.Link>
+          </Footer.LinkGroup>
         </div>
-    )
+        <Footer.Divider />
+        <Footer.Copyright href="https://nourish.com" by="Nourish™" year={2023} />
+      </div>
+    </Footer>
+  );
 }
