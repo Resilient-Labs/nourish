1b23979ac2446107ae6dcf6bbfd0d4188a67410a
socratic617 <erik.solis02135@gmail.com>
2023-12-24 14:08:21 -0500

    fe-erik FridgeList: connected to fridge  profile pages individually for each fridge on list

--- client/src/components/FridgeList.js
+++ client/src/components/FridgeList.js
@@ -7,21 +7,19 @@ export default function FridgeList({ allFridges }) {
         <div className="leaflet-container-list">
             <div>
                 {allFridges.fridges.map((item, index) => (
-                    <Card className="max-w-sm m-6" imgSrc={item.fridgeIMG ? item.fridgeIMG : item.pantryIMG} horizontal>
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <Card className="max-w-sm m-10" imgSrc={item.fridgeIMG ? item.fridgeIMG : item.pantryIMG} horizontal>
+                    <h2 className="text-2xl font-bold  text-gray-900 dark:text-white">
                         {item.name}
                     </h2>
                     <p className="font-normal text-gray-700 dark:text-gray-400">
                         <i className="fas fa-map-marker-alt pr-2" style={{ color: "#f17eb8" }}></i>
                         {item.location.address}, { }
-                        { item.location.cityState}
+                        {item.location.cityState}
                     </p>
-                    <Button href={item.location.directionsURL} size="sm"
-                        className="bg-pink-400 hover:bg-pink-500">
+                        <Button href={item.location.directionsURL} size="sm" style={{ width: "100%" }} className="bg-pink-400 hover:bg-pink-500">
                             Directions  
                     </Button>
-                    <Button href="#" size="sm"
-                        className="bg-pink-400 hover:bg-pink-500">
+                        <Button href={`/fridgeprofile/${item._id}`} size="sm" style={{ width: "100%" }} className="bg-pink-400 hover:bg-pink-500">
                             Learn More
                     </Button>
                 </Card>
