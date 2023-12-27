0ee7df92ce19832d714c160d3dbd03fe44d8cc7c
Brianna Willis <143905399+briannawillis195@users.noreply.github.com>
2023-12-27 13:46:58 -0500

    Revert "Finished 90% of the community page frontend with the help of Anais -"

--- client/src/pages/Community.js
+++ client/src/pages/Community.js
@@ -1,22 +1,8 @@
-import React from "react"
-import CommunityWelcome from "../components/WelcomeCommunity"
-import Navbar from "../components/Navbar"
-import CommunityInvolvement from "../components/CommunityInvolvement"
-import CommunityFooter from "../components/CommunityFooter"
-import MessageBoard from "../components/MessageBoardInfo"
-import MessageBoardInteractive from "../components/MessageBoardInteractive"
-
 export default function Community() {
-	return (
-		<div>
-			<Navbar />
-			<CommunityWelcome />
-			<MessageBoard />
-			<MessageBoardInteractive />
-			<h1>Unicorn</h1>
-			<h2>Hi</h2>
-			<CommunityInvolvement />
-			<CommunityFooter />
-		</div>
-	)
+  return (
+    <div>
+      <h1>Unicorn</h1>
+      <h2>Hi</h2>
+    </div>
+  )
 }
