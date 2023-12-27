41077590405d03483191ee397294385636eb74d0
JoseDiazCodes <josediazjr.a@gmail.com>
2023-12-27 14:44:51 -0500

    Resolved the issue with the revert// package.json was updated so make sure you npm install

--- client/src/pages/Community.js
+++ client/src/pages/Community.js
@@ -1,8 +1,22 @@
+import React from "react"
+import CommunityWelcome from "../components/WelcomeCommunity"
+import Navbar from "../components/Navbar"
+import CommunityInvolvement from "../components/CommunityInvolvement"
+import CommunityFooter from "../components/CommunityFooter"
+import MessageBoard from "../components/MessageBoardInfo"
+import MessageBoardInteractive from "../components/MessageBoardInteractive"
+
 export default function Community() {
-  return (
-    <div>
-      <h1>Unicorn</h1>
-      <h2>Hi</h2>
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<CommunityWelcome />
+			<MessageBoard />
+			<MessageBoardInteractive />
+			<h1>Unicorn</h1>
+			<h2>Hi</h2>
+			<CommunityInvolvement />
+			<CommunityFooter />
+		</div>
+	)
 }
