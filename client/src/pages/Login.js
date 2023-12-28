4f5f95d1c2c4ae8ab1d94452cadc95e961819c8d
Sarah <xsarahyu@gmail.com>
2023-12-28 12:20:49 -0500

    change login method to POST

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -14,7 +14,7 @@ export default function Login() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                 Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="POST">
+              <form className="space-y-4 md:space-y-6" method="POST" action="http://localhost:8000/login">
                 <div>
                   <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email address</label>
                   <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-300 focus:border-green-300 block w-full p-2.5" required />
