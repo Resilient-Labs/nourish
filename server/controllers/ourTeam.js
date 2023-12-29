ad35d9e5f5ea6eeba26dd578c6014e639a370b33
JacinthaDev <jacinthadev8@gmail.com>
2023-12-29 12:43:13 -0500

    Add controller for team member profiles and fix a post route

--- server/controllers/ourTeam.js
+++ server/controllers/ourTeam.js
@@ -2,6 +2,7 @@ import { cloudinary } from "../middleware/cloudinary.js"
 // import pkg from "mongodb"
 // const { ObjectID } = pkg
 
+
 export const getTeamMembers = async (req, res) => {
     try {
         const teamMembers = await teamMembers.find().sort({ firstName : "desc"}).lean()
