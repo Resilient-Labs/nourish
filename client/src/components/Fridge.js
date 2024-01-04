c8d8a0d1218a646fac98d3bbabfb5d355617ca5c
socratic617 <erik.solis02135@gmail.com>
2024-01-04 01:35:16 -0500

    fe-erik Fridge.js: silence code, commented code, styled to match pg, added attribute, imported searchbar component, added attributes to fridgeMap and FridgeList components

--- client/src/components/Fridge.js
+++ client/src/components/Fridge.js
@@ -1,51 +1,85 @@
 "use client"
+
+//modules
+import React, { useState, useEffect } from "react"
+
+//components
 import FridgeList from "./FridgeList.js"
 import FridgeMap from "./FridgeMap.js"
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import Searchbar from "./Searchbar.js"
 
 export default function Fridge() {
-  const [allFridges, setAllFridges] = useState([]);//changed it to array from originally object
+
+  //variables, setters
+  const [allFridges, setAllFridges] = useState([]);
+  const [filteredFridges, setupdatedFridges] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  //prop for updating list of fridges used for fridge list and fridge map
+  const updateFridges = (newFridgeData) => {
+    setupdatedFridges(newFridgeData);
+  };
+
+  //useEffect prevents multiple calls to the server for fridge list as react constantly is waiting and updating for changes
   useEffect(() => {
+
     const fetchAllFridges = async () => {
       try {
-        console.log("i am here now")
-        const response = await fetch(`http://localhost:8000/fridge/getAllFridges`)//connect to backend to pull DB due to the chain b-e makes request to be and f-e makes request to b-e server local 8000
-        console.log(response)
+
+        //get request to the server for list of fridges. Connect to b-e to pull from DB b/c f-e connect to localhost:3000
+        const response = await fetch(`http://localhost:8000/fridge/getAllFridges`)
         const data = await response.json();
-        console.log(" data i am here now pt 2")
-        console.log(data)
+
+        //use setters to update varialbes holding fridge data
         setAllFridges(data);
+        setupdatedFridges(data);
+
       } catch (error) {
         setError(error);
       } finally {
         setIsLoading(false);
       }
     };
+
+    //make server call
     fetchAllFridges();
   }, []);
 
+  //display when loading data
   if (isLoading) {
     return <p>Loading fridge list data...</p>;
   }
+
+  //display if error occurs when fetching data
   if (error) {
     return <p>Error fetching data: {error.message}</p>;
   }
-  console.log(allFridges)
+
   return (
     <div>
-      <h1 className="text-2xl font-bold text-center pt-8">Philadelphia Community Fridges and Pantries</h1>
+      {/* <h1 className="text-2xl text-gray-900 font-bold text-center pt-8">
+        
+      </h1> */}
+      <h2 className="text-center pt-14 text-4xl tracking-tight font-extrabold text-gray-900">Philadelphia Community Fridges and Pantries</h2>
+
+      {/* Component where users can search fridges by zipcode. Is passed the updateFridges prop so that it can return to the Fridge component filtered fridges which will be used by the FridgeList and FridgeMap component*/}
+      <Searchbar allFridges={allFridges} updateFridges={updateFridges} />
+
       <div className="flex p-12">
+
+        {/* Component that displays fridge data as cards. Is passed filteredFridges which is the output of the searchbar component filtering of fridges */}
         <div className="flex-1" style={{ marginRight: "2.5rem", backgroundColor: "rgb(250, 224, 158)" }}>
-          <FridgeList allFridges={allFridges} />
+          <FridgeList allFridges={filteredFridges} />
         </div>
+
+        {/* Component that displays an interactive map. Is passed filteredFridges which is the output of the searchbar component filtering of fridges */}
         <div className="lg:flex-1 block"> 
-          <FridgeMap allFridges={allFridges} />
+          <FridgeMap allFridges={filteredFridges} />
         </div>
+
       </div>
+
     </div>
   )
 }
